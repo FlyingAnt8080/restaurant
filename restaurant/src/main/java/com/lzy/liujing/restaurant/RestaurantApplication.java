37a338b3fbc7e75698cbfb0c79b4d03159e12d96
FlyingAnt8080 <18384623913@163.com>
2018-09-29 14:55:37 +0800

    create a project

---
+++ restaurant/src/main/java/com/lzy/liujing/restaurant/RestaurantApplication.java
@@ -0,0 +1,12 @@
+package com.lzy.liujing.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantApplication.class, args);
+	}
+}
---
+++ restaurant/src/test/java/com/lzy/liujing/restaurant/RestaurantApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lzy.liujing.restaurant;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestaurantApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
